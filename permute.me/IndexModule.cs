7803be89861a3af33e831aabac60324d19f9d96d
Christoph Enzmann <christoph.enzmann@confer.ch>
2013-09-13 14:03:24 +0200

    Initial Commit

---
+++ permute.me/IndexModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.Responses;
+
+namespace permute.me {
+
+    public class IndexModule : NancyModule {
+
+        public IndexModule() {
+            Get["/"] = _ => new RedirectResponse("/Content/html/site.html");
+        }
+
+    }
+}
---
+++ permute.me/NancyBootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace permute.me {
+    public class NancyBootstrapper : DefaultNancyBootstrapper {
+
+        protected override void ConfigureConventions(Nancy.Conventions.NancyConventions nancyConventions) {
+            //nancyConventions.StaticContentsConventions.Add(Nancy.Conventions.StaticContentConventionBuilder.AddDirectory("/Content"));
+            nancyConventions.StaticContentsConventions.Add(Nancy.Conventions.StaticContentConventionBuilder.AddDirectory("/Scripts"));
+            base.ConfigureConventions(nancyConventions);
+        }
+    }
+}
